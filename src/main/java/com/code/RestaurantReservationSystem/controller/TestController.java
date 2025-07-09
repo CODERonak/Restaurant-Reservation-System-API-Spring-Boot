81d3417ddc8a97f286687cbd57327142202ce6fc
r9988.2588 <r9988.2588@gmail.com>
2025-07-09 07:51:16 +0000

    first commit

---
+++ src/main/java/com/code/RestaurantReservationSystem/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.code.RestaurantReservationSystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Server running";
+    }
+}
