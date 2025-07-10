a945fe7b899d13e49f6ba89cd2fd008c8adcf8fe
r9988.2588 <r9988.2588@gmail.com>
2025-07-10 09:39:01 +0000

    added dtos for the login

---
+++ src/main/java/com/code/RestaurantReservationSystem/dto/Auth/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.code.RestaurantReservationSystem.dto.Auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
