2a8793e66c04dc5a38df91a4eee3ef77276e6a36
r9988.2588 <r9988.2588@gmail.com>
2025-07-12 09:22:00 +0000

    added table service

---
+++ src/main/java/com/code/RestaurantReservationSystem/service/TableService.java
@@ -0,0 +1,47 @@
+package com.code.RestaurantReservationSystem.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.code.RestaurantReservationSystem.dto.Table.TableDTO;
+import com.code.RestaurantReservationSystem.model.Restaurant;
+import com.code.RestaurantReservationSystem.model.Tables;
+import com.code.RestaurantReservationSystem.repository.TableRepository;
+
+@Service
+public class TableService {
+    private final TableRepository tableRepository;
+
+    public TableService(TableRepository tableRepository) {
+        this.tableRepository = tableRepository;
+    }
+
+    public void createTable(TableDTO request) {
+        Tables table = new Tables();
+        table.setTableNumber(request.getTableNumber());
+        table.setCapacity(request.getCapacity());
+        table.setStatus(request.getStatus());
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setId(request.getRestaurantId());
+        table.setRestaurant(restaurant);
+
+        tableRepository.save(table);
+    }
+
+    public void updateTable(Long tableId, TableDTO request) {
+        Tables existingTable = tableRepository.findById(tableId)
+                .orElseThrow(() -> new RuntimeException("Table not found with id: " + tableId));
+
+        existingTable.setTableNumber(request.getTableNumber());
+        existingTable.setCapacity(request.getCapacity());
+        existingTable.setStatus(request.getStatus());
+
+        tableRepository.save(existingTable);
+    }
+
+    public List<Tables> getAllTables(Restaurant restaurant) {
+        return tableRepository.findByRestaurant(restaurant);
+    }
+}
